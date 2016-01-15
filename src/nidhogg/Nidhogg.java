4d45975a3c4b404e11d95fe6f648bf3524ab1d04
Bellamy Matthieu <bellamy@a14p13.fil.univ-lille1.fr>
2016-01-15 17:14:04 +0100

    keyboard move

--- src/nidhogg/Nidhogg.java
+++ src/nidhogg/Nidhogg.java
@@ -1,35 +1,37 @@
 package nidhogg;
 
-import gameframework.drawing.GameCanvas;
-import gameframework.drawing.GameCanvasDefaultImpl;
-import gameframework.drawing.GameUniverseViewPort;
 import gameframework.drawing.GameUniverseViewPortDefaultImpl;
 import gameframework.game.GameConfiguration;
 import gameframework.game.GameData;
 import gameframework.game.GameLevel;
 import gameframework.game.GameLevelDefaultImpl;
 import gameframework.gui.GameWindow;
+import gameframework.motion.MoveStrategyKeyboard;
+import gameframework.motion.MoveStrategyKeyboard8Dir;
 
 public class Nidhogg {
+
 	public static void main(String[] args) {
 		GameConfiguration configuration = new GameConfiguration();
 		final GameData gameData = new GameData(configuration);
 		GameWindow gameWindow = new GameWindow("Nidhogg", gameData.getCanvas(), gameData);
+		
+		final MoveStrategyKeyboard strategyKeyBoard = new MoveStrategyKeyboard8Dir(false);
+		gameData.getCanvas().addKeyListener(strategyKeyBoard);
+		
 		gameWindow.createGUI();
 		
-		GameLevel level = new GameLevelDefaultImpl(gameData, 100) {
+		GameLevel level = new GameLevelDefaultImpl(gameData, 50) {
 			
 			@Override
 			protected void init() {
 				this.gameBoard = new GameUniverseViewPortDefaultImpl();
 				this.gameBoard.setGameData(gameData);
+				universe.addGameEntity(new Player(strategyKeyBoard));
 				System.out.println("dfgd");
 				gameData.getCanvas().setSize(640, 480);
 			}
 		};
-		
-		
-		
 		
 		
 		level.start();
