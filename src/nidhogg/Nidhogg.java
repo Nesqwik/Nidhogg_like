8cd6b4a236a565b2f37b3caffbeb70d943d517e8
Nesqwik <louis.guilbert@free.fr>
2016-01-15 14:55:08 +0100

    main window

--- src/nidhogg/Nidhogg.java
+++ src/nidhogg/Nidhogg.java
@@ -1,7 +1,18 @@
 package nidhogg;
 
+import gameframework.drawing.GameCanvas;
+import gameframework.drawing.GameCanvasDefaultImpl;
+import gameframework.game.GameConfiguration;
+import gameframework.game.GameData;
+import gameframework.gui.GameWindow;
+
 public class Nidhogg {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		GameCanvas gameCanvas = new GameCanvasDefaultImpl();
+		GameConfiguration configuration = new GameConfiguration();
+		GameData gameData = new GameData(configuration);
+		
+		GameWindow gameWindow = new GameWindow("Nidhogg", gameCanvas, gameData);
+		gameWindow.createGUI();
 	}
 }
