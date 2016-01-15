7e80da3c92e4d2f9d30480ec83fd3d6861e36a35
Damien M <dma.truc@gmail.com>
2016-01-15 15:53:19 +0100

    Gestion de l'image de fond

--- src/nidhogg/Nidhogg.java
+++ src/nidhogg/Nidhogg.java
@@ -2,17 +2,37 @@ package nidhogg;
 
 import gameframework.drawing.GameCanvas;
 import gameframework.drawing.GameCanvasDefaultImpl;
+import gameframework.drawing.GameUniverseViewPort;
+import gameframework.drawing.GameUniverseViewPortDefaultImpl;
 import gameframework.game.GameConfiguration;
 import gameframework.game.GameData;
+import gameframework.game.GameLevel;
+import gameframework.game.GameLevelDefaultImpl;
 import gameframework.gui.GameWindow;
 
 public class Nidhogg {
 	public static void main(String[] args) {
-		GameCanvas gameCanvas = new GameCanvasDefaultImpl();
 		GameConfiguration configuration = new GameConfiguration();
-		GameData gameData = new GameData(configuration);
-		
-		GameWindow gameWindow = new GameWindow("Nidhogg", gameCanvas, gameData);
+		final GameData gameData = new GameData(configuration);
+		GameWindow gameWindow = new GameWindow("Nidhogg", gameData.getCanvas(), gameData);
 		gameWindow.createGUI();
+		
+		GameLevel level = new GameLevelDefaultImpl(gameData, 100) {
+			
+			@Override
+			protected void init() {
+				this.gameBoard = new GameUniverseViewPortDefaultImpl();
+				this.gameBoard.setGameData(gameData);
+				System.out.println("dfgd");
+				gameData.getCanvas().setSize(640, 480);
+			}
+		};
+		
+		
+		
+		
+		
+		level.start();
+		
 	}
 }
