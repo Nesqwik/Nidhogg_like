5a7dd363692f08aa817d51ac828ae4051cf59b59
Jonathan <jonathan.lecointe@gmail.com>
2016-01-16 11:43:28 +0100

    Mavenization (WIP)

--- src/main/java/nidhogglike/Nidhogg.java
+++ src/main/java/nidhogglike/Nidhogg.java
@@ -1,4 +1,4 @@
-package nidhogg;
+package nidhogglike;
 
 import gameframework.drawing.GameUniverseViewPortDefaultImpl;
 import gameframework.game.GameConfiguration;
@@ -6,35 +6,24 @@ import gameframework.game.GameData;
 import gameframework.game.GameLevel;
 import gameframework.game.GameLevelDefaultImpl;
 import gameframework.gui.GameWindow;
-import gameframework.motion.MoveStrategyKeyboard;
-import gameframework.motion.MoveStrategyKeyboard8Dir;
 
 public class Nidhogg {
-
-	public static void main(String[] args) {
-		GameConfiguration configuration = new GameConfiguration();
+	public static void main(final String[] args) {
+		final GameConfiguration configuration = new GameConfiguration();
 		final GameData gameData = new GameData(configuration);
-		GameWindow gameWindow = new GameWindow("Nidhogg", gameData.getCanvas(), gameData);
-		
-		final MoveStrategyKeyboard strategyKeyBoard = new MoveStrategyKeyboard8Dir(false);
-		gameData.getCanvas().addKeyListener(strategyKeyBoard);
-		
+		final GameWindow gameWindow = new GameWindow("Nidhogg", gameData.getCanvas(), gameData);
 		gameWindow.createGUI();
-		
-		GameLevel level = new GameLevelDefaultImpl(gameData, 50) {
-			
+
+		final GameLevel level = new GameLevelDefaultImpl(gameData, 100) {
+
 			@Override
 			protected void init() {
 				this.gameBoard = new GameUniverseViewPortDefaultImpl();
 				this.gameBoard.setGameData(gameData);
-				universe.addGameEntity(new Player(strategyKeyBoard));
 				System.out.println("dfgd");
 				gameData.getCanvas().setSize(640, 480);
 			}
 		};
-		
-		
 		level.start();
-		
 	}
 }
