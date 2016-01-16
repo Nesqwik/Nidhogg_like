7c1255a72b6d99097d13e31262f3a281c0c86f13
Jonathan <jonathan.lecointe@gmail.com>
2016-01-16 12:55:30 +0100

    Game working with maven

--- src/main/java/nidhogglike/Nidhogg.java
+++ src/main/java/nidhogglike/Nidhogg.java
@@ -6,12 +6,18 @@ import gameframework.game.GameData;
 import gameframework.game.GameLevel;
 import gameframework.game.GameLevelDefaultImpl;
 import gameframework.gui.GameWindow;
+import gameframework.motion.MoveStrategyKeyboard;
+import gameframework.motion.MoveStrategyKeyboard8Dir;
 
 public class Nidhogg {
 	public static void main(final String[] args) {
 		final GameConfiguration configuration = new GameConfiguration();
 		final GameData gameData = new GameData(configuration);
 		final GameWindow gameWindow = new GameWindow("Nidhogg", gameData.getCanvas(), gameData);
+
+		final MoveStrategyKeyboard keyboard = new MoveStrategyKeyboard8Dir(false);
+		gameData.getCanvas().addKeyListener(keyboard);
+
 		gameWindow.createGUI();
 
 		final GameLevel level = new GameLevelDefaultImpl(gameData, 100) {
@@ -20,7 +26,7 @@ public class Nidhogg {
 			protected void init() {
 				this.gameBoard = new GameUniverseViewPortDefaultImpl();
 				this.gameBoard.setGameData(gameData);
-				System.out.println("dfgd");
+				universe.addGameEntity(new Player(keyboard));
 				gameData.getCanvas().setSize(640, 480);
 			}
 		};
