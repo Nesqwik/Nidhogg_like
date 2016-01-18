518b0888913c77a830ed64016ef59a932a94a1cb
Damien M <dma.truc@gmail.com>
2016-01-18 15:15:35 +0100

    Better organization

--- src/nidhogg/Nidhogg.java
+++ src/nidhogg/Nidhogg.java
@@ -1,40 +1,52 @@
 package nidhogg;
 
+import nidhogg.entities.Player;
 import gameframework.drawing.GameUniverseViewPortDefaultImpl;
 import gameframework.game.GameConfiguration;
 import gameframework.game.GameData;
-import gameframework.game.GameLevel;
 import gameframework.game.GameLevelDefaultImpl;
 import gameframework.gui.GameWindow;
 import gameframework.motion.MoveStrategyKeyboard;
 import gameframework.motion.MoveStrategyKeyboard8Dir;
 
-public class Nidhogg {
+/**
+ * @author Team 2
+ * 
+ * Main class of the game
+ * 
+ * Create the game window, canvas and entities
+ * then start the main loop
+ */
+public class Nidhogg extends GameLevelDefaultImpl {
+	public static int WIDTH = 640;
+	public static int HEIGHT = 480;
+	public static int SPRITE_SIZE = 16;
+	public static int FPS = 60;
+	
+	public Nidhogg(GameData gameData) {
+		super(gameData, (int) (1000f / FPS));
+	}
 
+	/** (non-Javadoc)
+	 * @see gameframework.game.GameLevelDefaultImpl#init()
+	 */
+	@Override
+	protected void init() {
+		final MoveStrategyKeyboard strategyKeyBoard = new MoveStrategyKeyboard8Dir(false);
+		data.getCanvas().addKeyListener(strategyKeyBoard);
+		this.gameBoard = new GameUniverseViewPortDefaultImpl();
+		this.gameBoard.setGameData(data);
+		
+		universe.addGameEntity(new Player(strategyKeyBoard));
+	}
+	
+	
 	public static void main(String[] args) {
-		GameConfiguration configuration = new GameConfiguration();
+		GameConfiguration configuration = new GameConfiguration(HEIGHT / SPRITE_SIZE, WIDTH / SPRITE_SIZE, SPRITE_SIZE, 42);
 		final GameData gameData = new GameData(configuration);
 		GameWindow gameWindow = new GameWindow("Nidhogg", gameData.getCanvas(), gameData);
 		
-		final MoveStrategyKeyboard strategyKeyBoard = new MoveStrategyKeyboard8Dir(false);
-		gameData.getCanvas().addKeyListener(strategyKeyBoard);
-		
 		gameWindow.createGUI();
-		
-		GameLevel level = new GameLevelDefaultImpl(gameData, 50) {
-			
-			@Override
-			protected void init() {
-				this.gameBoard = new GameUniverseViewPortDefaultImpl();
-				this.gameBoard.setGameData(gameData);
-				universe.addGameEntity(new Player(strategyKeyBoard));
-				System.out.println("dfgd");
-				gameData.getCanvas().setSize(640, 480);
-			}
-		};
-		
-		
-		level.start();
-		
+		new Nidhogg(gameData).start();
 	}
 }
