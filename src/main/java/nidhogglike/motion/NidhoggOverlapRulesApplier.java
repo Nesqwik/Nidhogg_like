593e871b59a9dc614d9e873b5618ae6b7bdf963d
Damien M <dma.truc@gmail.com>
2016-01-30 14:05:43 +0100

    Fix a bug where the sword was hitting the player even if it was planted in the ground

--- src/main/java/nidhogglike/motion/NidhoggOverlapRulesApplier.java
+++ src/main/java/nidhogglike/motion/NidhoggOverlapRulesApplier.java
@@ -19,27 +19,31 @@ public class NidhoggOverlapRulesApplier extends OverlapRulesApplierDefaultImpl {
 		if (sword.getHolder() == player) {
 			return;
 		}
-		boolean mustKill = false;
+		boolean mustHit = false;
 
 		if (sword.isMoving()) {
-			mustKill = true;
+			mustHit = true;
 			if (sword.getLastHolder() == player)
 				announcer.registerSuicide(player);
 			else
 				announcer.registerKill(sword.getLastHolder());
 		} else if (sword.getHolder() != null) {
-			mustKill = player.isKilledBy(sword.getHolder());
+			mustHit = player.isKilledBy(sword.getHolder());
 			
 			announcer.registerKill(sword.getHolder());
 		} else if (!player.isHoldingSword()) {
 			player.setSword(sword);
 		}
 		
-		if (mustKill && !sword.isHeld()) {
-			player.emitParticle();
-			player.die();
-		} else {
-			mustKill = player.hit();
+		boolean mustKill = false;
+		if (mustHit) {
+			if (!sword.isHeld()) {
+				player.emitParticle();
+				player.die();
+				mustKill = true;
+			} else {
+				mustKill = player.hit();
+			}
 		}
 
 		if (mustKill) {
