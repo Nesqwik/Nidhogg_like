5e6437f87334cf370b31346675a6bbfbb91e03e0
Damien M <dma.truc@gmail.com>
2016-01-30 14:19:07 +0100

    Improved lifebar style

--- src/main/java/nidhogglike/entities/LifeBar.java
+++ src/main/java/nidhogglike/entities/LifeBar.java
@@ -11,6 +11,10 @@ import nidhogglike.motion.NidhoggMovable;
 public class LifeBar extends NidhoggMovable implements GameEntity {
 	protected Player player;
 	
+	private static Color BACKGROUND_COLOR = new Color(60, 60, 60);
+	private static Color LIFE_COLOR1 = new Color(215, 74, 74);
+	private static Color LIFE_COLOR2 = new Color(208, 47, 47);
+	
 	private static int WIDTH = 50;
 	
 	public LifeBar(int maxLife, Player player) {
@@ -21,12 +25,16 @@ public class LifeBar extends NidhoggMovable implements GameEntity {
 	public void draw(Graphics g) {
 		this.getPosition().x = (player.getBoundingBox().width - WIDTH) / 2 + player.getPosition().x;
 		this.getPosition().y =  player.getPosition().y - 15;
+		int lifeWidth = (int) (((player.getCurrentLife() + 0f) / player.getMaxLife())* WIDTH) - 4;
 		
-		g.setColor(Color.BLACK);
+		g.setColor(BACKGROUND_COLOR);
 		g.fillRect(this.getPosition().x, this.getPosition().y, WIDTH, 9);
 		
-		g.setColor(Color.RED);
-		g.fillRect(this.getPosition().x + 2, this.getPosition().y + 2, (int) (((player.getCurrentLife() + 0f) / player.getMaxLife())* WIDTH) - 4, 5);
+		g.setColor(LIFE_COLOR1);
+		g.fillRect(this.getPosition().x + 2, this.getPosition().y + 2, lifeWidth, 2);
+		
+		g.setColor(LIFE_COLOR2);
+		g.fillRect(this.getPosition().x + 2, this.getPosition().y + 4, lifeWidth, 3);
 	}
 
 	@Override
