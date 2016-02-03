612fb5897ff51a4868e30be92a1b4d42be9c8f06
meyer <helene.meyer@univ-lille1.fr>
2016-02-04 00:27:14 +0100

    test NidhoggOverlapRulesApplier ok

--- src/test/java/nidhogglike/motion/NidhoggOverlapRulesApplierTest.java
+++ src/test/java/nidhogglike/motion/NidhoggOverlapRulesApplierTest.java
@@ -2,6 +2,7 @@ package nidhogglike.motion;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.awt.Point;
@@ -17,7 +18,6 @@ import nidhogglike.input.Input;
 import nidhogglike.particles.ParticleEmitter;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class NidhoggOverlapRulesApplierTest {
@@ -34,32 +34,24 @@ public class NidhoggOverlapRulesApplierTest {
 	public void init() {
 		data = new NidhoggGameData(new NidhoggConfiguration(0, 0, 0, 0));
 		data.setObservableValue(Nidhogg.PLAYER1_DATA_KEY, 0);
+		data.setObservableValue(Nidhogg.PLAYER2_DATA_KEY, 0);
 		sword = new Sword(data, true);
 		sword2 = new Sword(data, false);
 		player = new Player(data, new Input(data.getCanvas()), true);
 		player.setParticleEmitter(new ParticleEmitter());
 		player2 = new Player(data, new Input(data.getCanvas()), false);
-		sword.setHolder(player);
-		sword2.setHolder(player2);
+		player2.setParticleEmitter(new ParticleEmitter());
 		surpriseGift = new SurpriseGift(data);
 		NidhoggAnnouncer announcer = ((NidhoggConfiguration) data.getConfiguration()).getAnnouncer();
 		announcer.addPlayer(player);
 		announcer.addPlayer(player2);
-		rulesOverlap = (NidhoggOverlapRulesApplier) ((NidhoggConfiguration) data.getConfiguration()).createOverlapRulesApplier();
+		rulesOverlap = new NidhoggOverlapRulesApplier(announcer);
 	}
 
 	@Test
 	public void testOverlapRuleSwordPlayerWhenHolderIsPlayer() {
 		player.setSword(sword);
 		rulesOverlap.overlapRule(sword, player);
-	}
-	
-	@Test
-	public void testOverlapRuleSwordPlayer() {
-		data.getObservableValue(Nidhogg.PLAYER1_DATA_KEY).setValue(2);
-		sword.setMoving(true);
-		rulesOverlap.overlapRule(sword, player);
-		assertEquals(3, (int)data.getObservableValue(Nidhogg.PLAYER1_DATA_KEY).getValue());
 	}
 
 	@Test
@@ -79,17 +71,74 @@ public class NidhoggOverlapRulesApplierTest {
 	}
 	
 	@Test
+	public void testOverlapRuleSwordWhenPlayerIsInvincible() {
+		player.setCurrentLife(3);
+		data.getObservableValue(Nidhogg.PLAYER1_DATA_KEY).setValue(2);
+		sword2.setHolder(player2);
+		player.setMaxInvincibleLife(1000);
+		player.invincible();
+		assertTrue(player.stillInvincible());
+		rulesOverlap.overlapRule(sword, player);
+		assertEquals(3, player.getCurrentLife());
+		assertEquals(2, (int)data.getObservableValue(Nidhogg.PLAYER1_DATA_KEY).getValue());
+	}
+	
+	@Test
 	public void testOverlapRuleSwordPlayerWithBonus() {
 		data.getObservableValue(Nidhogg.PLAYER1_DATA_KEY).setValue(2);
-		player2.setSword(sword);
+		player2.setSword(sword2);
 		player2.swordStronger();
 		assertEquals(3, player2.getStrongerSword());
-//		TODO Problem Announcer !!
-//		
-//		rulesOverlap.overlapRule(sword, player);
-//		assertEquals(3, (int)data.getObservableValue(Nidhogg.PLAYER1_DATA_KEY).getValue());
-//		assertEquals(2, player2.getStrongerSword());
-//		assertEquals(3, player.getCurrentLife());
+		assertNull(player.getSword());
+		rulesOverlap.overlapRule(sword2, player);
+		assertEquals(3, (int)data.getObservableValue(Nidhogg.PLAYER1_DATA_KEY).getValue());
+		assertEquals(2, player2.getStrongerSword());
+		assertEquals(3, player.getCurrentLife());
+	}
+	
+	@Test
+	public void testOverlapRuleSwordPlayerWithBonusAndTakeHisSword() {
+		data.getObservableValue(Nidhogg.PLAYER2_DATA_KEY).setValue(2);
+		player2.setCurrentLife(3);
+		player2.swordStronger();
+		assertEquals(3, player2.getStrongerSword());
+		assertNull(sword2.getHolder());
+		assertNull(player2.getSword());
+		rulesOverlap.overlapRule(sword2, player2);
+		assertEquals(2, (int)data.getObservableValue(Nidhogg.PLAYER2_DATA_KEY).getValue());
+		assertEquals(3, player2.getStrongerSword());
+		assertEquals(3, player.getCurrentLife());
+		assertEquals(sword2, player2.getSword());
+	}
+	
+	@Test
+	public void testSameDirectionPlayers() {
+		player.setSword(sword);
+		player2.setCurrentLife(2);
+		sword.setMoving(false);
+		rulesOverlap.overlapRule(sword, player2);
+		assertEquals(2, player2.getCurrentLife());
+	}
+	
+	@Test
+	public void testOppositeDirectionPlayers() {
+		player.setSword(sword);
+		Player player3 = new Player(data, new Input(data.getCanvas()), true);
+		player3.setCurrentLife(2);
+		player3.setParticleEmitter(new ParticleEmitter());
+		sword.setMoving(false);
+		rulesOverlap.overlapRule(sword, player3);
+		assertEquals(1, player3.getCurrentLife());
+	}
+	
+	@Test
+	public void testOverlapRuleSwordPlayerWhenHeCanBeDead() {
+		data.getObservableValue(Nidhogg.PLAYER1_DATA_KEY).setValue(2);
+		player.setCurrentLife(1);
+		player2.setSword(sword2);
+		rulesOverlap.overlapRule(sword2, player);
+		assertEquals(3, player.getCurrentLife());
+		assertEquals(3, (int)data.getObservableValue(Nidhogg.PLAYER1_DATA_KEY).getValue());
 	}
 	
 	@Test
@@ -114,6 +163,15 @@ public class NidhoggOverlapRulesApplierTest {
 	}
 	
 	@Test
+	public void testOverlapRuleSwordThrowAndTake() {
+		player.setSword(sword);
+		sword.playerThrow();
+		assertNull(sword.getHolder());
+		rulesOverlap.overlapRule(sword, player);
+		assertEquals(sword, player.getSword());
+	}
+	
+	@Test
 	public void testOverlapRuleSwordSword3() {
 		player.setSword(sword2);
 		assertTrue(sword2.isHeld());
@@ -128,11 +186,6 @@ public class NidhoggOverlapRulesApplierTest {
 		surpriseGift.setGift(0, player);
 		rulesOverlap.overlapRule(surpriseGift, player);
 		assertTrue(surpriseGift.isOpened());
-	}
-
-	@Test
-	public void testOverlapRuleHeadBalloonPlayer() {
-		//TODO
 	}
 
 }
